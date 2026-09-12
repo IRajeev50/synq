c1fe05aedb8290fdde790649d32432d53292e4c3
Rajeev Singh <37499501+IRajeev50@users.noreply.github.com>
2026-09-12 16:04:24 +0530

    Add onState listener to SynqNearby module

--- apps/mobile/modules/synq-nearby/src/index.js
+++ apps/mobile/modules/synq-nearby/src/index.js
@@ -1,3 +1,3 @@
 import {EventEmitter,requireNativeModule} from 'expo-modules-core';
 const Native=requireNativeModule('SynqNearby');const emitter=new EventEmitter(Native);
-export const SynqNearby={permissionState:()=>Native.permissionState(),start:token=>Native.start(token),stop:()=>Native.stop(),onEncounter:handler=>emitter.addListener('onEncounter',handler)};
+export const SynqNearby={permissionState:()=>Native.permissionState(),start:token=>Native.start(token),stop:()=>Native.stop(),onEncounter:handler=>emitter.addListener('onEncounter',handler),onState:handler=>emitter.addListener('onState',handler)};
